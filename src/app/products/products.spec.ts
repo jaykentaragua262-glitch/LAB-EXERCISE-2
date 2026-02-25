827de677576bffa22f30f5160ee93523b4a7eef9
Your Name <you@example.com>
2026-02-25 15:41:37 +0800

    Lab-3---Dynamic-products-with-Angular

--- src/app/products/products.spec.ts
+++ src/app/products/products.spec.ts
@@ -1,19 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProductsComponent } from './products';
 
-import { Products } from './products';
-
-describe('Products', () => {
-  let component: Products;
-  let fixture: ComponentFixture<Products>;
+describe('ProductsComponent', () => {
+  let component: ProductsComponent;
+  let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Products]
-    })
-    .compileComponents();
+      imports: [ProductsComponent]
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(Products);
+    fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
